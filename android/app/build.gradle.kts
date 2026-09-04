181f1c2d27ea5ecd287a9ecaeed271d31a419580
arshad-dev57 <qureshiarshad947@gmail.com>
2026-09-04 19:18:10 +0500

    worked on the side  of deletion so that i am getting

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,6 +11,9 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
+        // Enable core library desugaring for flutter_local_notifications
+        isCoreLibraryDesugaringEnabled = true
+
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
@@ -37,6 +40,10 @@ android {
             signingConfig = signingConfigs.getByName("debug")
         }
     }
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.5")
 }
 
 flutter {
